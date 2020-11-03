c25e7eb49ae47d95d3dbbdce365b61c86b9cf034
quest101 <2006xikang101@163.com>
2020-11-03 20:16:29 +0800

    commit

--- feign-consumer/src/main/java/con/ken/controller/ConsumerController.java
+++ feign-consumer/src/main/java/con/ken/controller/ConsumerController.java
@@ -1,19 +1,55 @@
 package con.ken.controller;
 
-import con.ken.service.HelloService;
+import com.ken.dto.User;
+import con.ken.service.RefactorHelloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/feign")
 public class ConsumerController {
 
-    @Autowired
-    HelloService helloService;
+    private final static Logger logger = LoggerFactory.getLogger(ConsumerController.class);
 
-    @RequestMapping(value = "/feign-consumer",method = RequestMethod.GET)
+   // @Autowired
+    //HelloService1 helloService;
+//
+    @Autowired
+    RefactorHelloService refactorHelloService;
+
+/*    @GetMapping("/hello")
     public String helloConsumer(){
-        return helloService.hello();
+        return "HELLO WORLD";
+    }*/
+//
+//    //@RequestMapping(value = "/feign-consumer",method = RequestMethod.GET)
+//    @GetMapping("/feign-consumer")
+//    public String helloConsumer(){
+//        return helloService.hello();
+//    }
+//
+//    @GetMapping("/feign-consumer2")
+//    public String helloConsumer2(){
+//        StringBuilder sb = new StringBuilder();
+//        sb.append(helloService.hello()).append("\n");
+//        sb.append(helloService.hello("DIDI")).append("\n");
+//        sb.append(helloService.hello("DIDI",18)).append("\n");
+//        sb.append(helloService.hello(new User("DIDI",18))).append("\n");
+//        return sb.toString();
+//    }
+
+    @GetMapping("/consumer")
+    public String helloConsumer3(){
+        logger.info("==============feign-consumer============");
+        StringBuilder sb = new StringBuilder();
+        //sb.append(helloService.hello()).append("\n");
+        sb.append(refactorHelloService.hello("DIDI")).append("\n");
+        sb.append(refactorHelloService.hello("DIDI",18)).append("\n");
+        sb.append(refactorHelloService.hello(new User("DIDI",18))).append("\n");
+        return sb.toString();
     }
 }
---
+++ feign-consumer/src/main/java/con/ken/service/HelloService2.java
@@ -0,0 +1,18 @@
+package con.ken.service;
+
+//@FeignClient(value = "hello-service")
+public interface HelloService2 {
+
+//    @RequestMapping("/api/ribbon/index")
+//    String hello();
+//
+//    @RequestMapping("/api/ribbon/index1")
+//    String hello(@RequestParam("name") String name);
+//
+//    @RequestMapping("/api/ribbon/index2")
+//    String hello(@RequestHeader("name") String name,
+//                 @RequestHeader("age") Integer age);
+//
+//    @RequestMapping("/api/ribbon/index3")
+//    String hello(@RequestBody User user);
+}
